c3e2ac6a7458efc52ac06a959cb8d8ddc043c8f2
Luca Giozza <luca.giozza@gmail.com>
2021-09-06 14:56:51 +0200

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,33 @@
+<header>
+  <div class="header-superiore">
+
+  </div>
+  <div class="header-centrale">
+      <div class="logo">
+          <img src="" alt="">
+      </div>
+      <div class="menu">
+          <nav>
+              <ul>
+                  <li><a href="{{ route('personaggi')}}">characters</a></li>
+                  <li><a href="">comics</a></li>
+                  <li><a href="">movies</a></li>
+                  <li><a href="">tv</a></li>
+                  <li><a href="">games</a></li>
+                  <li><a href="">collectibles</a></li>
+                  <li><a href="">videos</a></li>
+                  <li><a href="">fans</a></li>
+                  <li><a href="">news</a></li>
+                  <li><a href="">shop</a></li>
+              </ul>
+          </nav>
+
+      </div>
+
+  </div>
+
+  <div class="header-inferiore">
+
+  </div>
+
+</header>
